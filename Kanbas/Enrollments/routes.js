89190a96a22da09e791f526c68f86cd56308cf63
5jayarama <54865529+5jayarama@users.noreply.github.com>
2024-12-04 23:16:30 -0500

    fixed minor enrollment bug

--- Kanbas/Enrollments/routes.js
+++ Kanbas/Enrollments/routes.js
@@ -1,6 +1,11 @@
 import * as enrollmentsDao from "./dao.js";
 
 export default function EnrollmentsRoutes(app) {
+    app.post("/api/enrollments", (req, res) => {
+        const { userId, courseId } = req.body;
+        enrollmentsDao.addEnrollment(userId, courseId);
+        res.status(201).end();
+    });
     app.delete("/api/enrollments/:enrollmentId", (req, res) => {
         const { enrollmentId } = req.params;
         enrollmentsDao.removeEnrollment(enrollmentId);
